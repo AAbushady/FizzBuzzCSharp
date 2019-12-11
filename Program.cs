230203996cafa00924f77f9def01833eaa9f8c31
Alex Abushady <aabushady214@gmail.com>
2019-12-11 08:41:40 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace FizzBuzz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            const int LoopAmount = 100;
+
+            for (int cnt = 1; cnt <= LoopAmount; cnt++)
+            {
+                FizzBuzzer(cnt);
+            }
+        }
+
+        public static void FizzBuzzer(int cnt)
+        {
+            if (cnt % 3 == 0 && cnt % 5 == 0)
+            {
+                Console.WriteLine("FizzBuzz");
+            }
+            else if (cnt % 3 == 0)
+            {
+                Console.WriteLine("Fizz");
+            }
+            else if (cnt % 5 == 0)
+            {
+                Console.WriteLine("Buzz");
+            }
+            else
+            {
+                Console.WriteLine(cnt);
+            }
+        }
+    }
+}
